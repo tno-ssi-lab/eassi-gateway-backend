109d4f8b9bb9a18f034e5b48c173ba5ea3eae614
Hidde-Jan Jongsma <hidde-jan.jongsma@tno.nl>
2020-11-06 12:45:02 +0100

    Add webhook for indy agent

--- src/connectors/indy/indy.controller.ts
+++ src/connectors/indy/indy.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { IndyService } from './indy.service';
 import { CreateIndySchemaDto } from './create-indy-schema.dto';
 
@@ -14,5 +14,11 @@ export class IndyController {
   @Post()
   create(@Body() indySchemaData: CreateIndySchemaDto) {
     return this.indyService.createSchema(indySchemaData);
+  }
+
+  @Post('webhook/topic/:topic/')
+  webhook(@Param('topic') topic: string, @Body() body: any) {
+    console.log('Incomming indy webhook', topic, body);
+    // TODO: Implement callbacks for accepted connections and issued credentials.
   }
 }
