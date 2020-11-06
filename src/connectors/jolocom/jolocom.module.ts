db9b84369baeaae47419b4b4674d1afd66cfe889
Hidde-Jan Jongsma <hidde-jan.jongsma@tno.nl>
2020-11-06 12:45:49 +0100

    Create JolocomSDK injectable

--- src/connectors/jolocom/jolocom.module.ts
+++ src/connectors/jolocom/jolocom.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JolocomSDK } from '@jolocom/sdk';
+import { JolocomTypeormStorage } from '@jolocom/sdk-storage-typeorm';
 
 import { JolocomService } from './jolocom.service';
 import { JolocomWallet } from './jolocom-wallet.entity';
 import { JolocomCredentialType } from './jolocom-credential-type.entity';
 import { JolocomController } from './jolocom.controller';
 import { JolocomCredentialRequestToken } from './jolocom-credential-request-token.entity';
+import { Connection } from 'typeorm';
+
+const jolocomSDKFactory = {
+  provide: JolocomSDK,
+  useFactory: (connection: Connection) => {
+    return new JolocomSDK({
+      storage: new JolocomTypeormStorage(connection),
+    });
+  },
+  inject: [Connection],
+};
 
 @Module({
   imports: [
@@ -15,7 +28,7 @@ import { JolocomCredentialRequestToken } from './jolocom-credential-request-toke
       JolocomCredentialRequestToken,
     ]),
   ],
-  providers: [JolocomService],
+  providers: [JolocomService, jolocomSDKFactory],
   exports: [JolocomService, TypeOrmModule],
   controllers: [JolocomController],
 })
