2d82ea2dc56ca64b40c5c1590b0b80ab58427ca3
Kevin Witlox <kevin.witlox@tno.nl>
2023-03-13 16:39:12 +0100

    Expose Jolocom environment variables
    Expose the JOLOCOM_PROVIDER_URL and JOLOCOM_CONTRACT_ADDRESS
    environement variable.
    
    JOLOCOM_PROVIDER_URL allows one to specify an alternative Ethereum
    (Testnet) Node Provider.
    JOLOCOM_CONTRACT_ADDRESS allows one to specify an alternative address
    for the Jolocom Registry smart contract.

--- src/connectors/jolocom/jolocom.module.ts
+++ src/connectors/jolocom/jolocom.module.ts
@@ -13,7 +13,12 @@ import { DidMethodKeeper } from '@jolocom/sdk/js/didMethodKeeper';
 import { JoloDidMethod } from 'jolocom-lib/js/didMethods/jolo';
 
 const PROVIDER_URL =
+  process.env.JOLOCOM_PROVIDER_URL ||
   'https://rinkeby.infura.io/v3/683ec89a2ede42f8a29842b5ebbef450';
+const CONTRACT_ADDRESS =
+  process.env.JOLOCOM_CONTRACT_ADDRESS ||
+  // Default contract address (see github jolocom/jolo-did-method, 'packages/jolo-did-registrar/ts/index.ts')
+  '0xd4351c3f383d79ba378ed1875275b1e7b960f120';
 
 const jolocomSDKFactory = {
   provide: JolocomSDK,
@@ -22,8 +27,8 @@ const jolocomSDKFactory = {
       storage: new JolocomTypeormStorage(connection),
     });
 
-    // Use custom rinkeby endpoint
-    const jolo = new JoloDidMethod(PROVIDER_URL);
+    // Use custom testnet endpoint
+    const jolo = new JoloDidMethod(PROVIDER_URL, CONTRACT_ADDRESS);
     const mk = new DidMethodKeeper(jolo);
     mk.register('jun', sdk.didMethods.get('jun'));
     sdk.didMethods = mk;
